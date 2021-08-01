136e56411c20b705a3699c119e0f46bc3273aedb
manjukrishn <58678684+manjukrishn@users.noreply.github.com>
2021-08-01 15:48:16 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,12 +1,5 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyBo9yykhBg5KwwKZy1OPaIXRdGkcUHQcdo",
-  authDomain: "liny-de878.firebaseapp.com",
-  databaseURL: "https://liny-de878.firebaseio.com",
-  projectId: "liny-de878",
-  storageBucket: "liny-de878.appspot.com",
-  messagingSenderId: "24484947972",
-  appId: "1:24484947972:web:17c16cf1908fa727edad51"
 };
 const firebaseApp = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
